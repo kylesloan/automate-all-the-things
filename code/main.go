42abf50f23c32ecb8f8a150508a1a2f076d2ddca
Kyle Sloan <kyle.sloan@me.com>
2019-08-02 16:39:28 -0500

    add http header to go script to show you are bing load balanced

--- code/main.go
+++ code/main.go
@@ -1,14 +1,25 @@
 package main
 
+// packages to use https://medium.com/golangspec/import-declarations-in-go-8de0fd3ae8ff
 import (
   "fmt"
   "log"
   "net/http"
   "time"
+  "os"
 )
 
+// get the hostname so we can add it to a http header to see that we are getting load balanced
+func getName() string {
+  name, err := os.Hostname()
+  if err != nil { panic(err) }
+  return name
+}
+
+// http object with now timestamp
 func echoTime(w http.ResponseWriter, r *http.Request) {
   time := time.Now().Unix()
+  w.Header().Set("ServedBy", getName())
   fmt.Fprintf(w, "{ \"message\": \"Automate all the things!\", \"timestamp\": %v }", time)
 }
 
